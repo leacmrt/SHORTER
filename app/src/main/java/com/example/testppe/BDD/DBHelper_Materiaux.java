638f3549732ea4c544ede33b5ab7e5140dcd1b01
leacmrt <lea.commaret@edu.ece.fr>
2021-04-03 12:19:54 +0200

    toujours BDD c'est long

--- app/src/main/java/com/example/testppe/BDD/DBHelper_Materiaux.java
+++ app/src/main/java/com/example/testppe/BDD/DBHelper_Materiaux.java
@@ -1,4 +1,74 @@
 package com.example.testppe.BDD;
 
-public class DBHelper_Materiaux {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+public class DBHelper_Materiaux  extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "MyDBName.db_pste1";
+    public static final String PROJET_TABLE_NAME = "Materiaux";
+    Context myContext;
+    SQLiteDatabase myDataBase;
+    private HashMap hp;
+
+    public DBHelper_Materiaux(Context context) {
+
+        super(context, DATABASE_NAME , null, 4);
+        myContext = context;
+        myDataBase = this.getReadableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table Materiaux" +
+                        "(id integer, nom text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+
+        if(newVersion>oldVersion)
+        {onCreate(db);}
+
+        db.execSQL("DROP TABLE IF EXISTS Materiaux");
+        onCreate(db);
+    }
+
+    public void insertMateriaux (int id_util, String re) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id",id_util);
+        contentValues.put("avis",re);
+        db.insert("Materiaux", null, contentValues);
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from Materiaux where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, PROJET_TABLE_NAME);
+        return numRows;
+    }
+
+
+    public Integer deleteMateriaux (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("Materiaux",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
 }
--- app/src/main/java/com/example/testppe/BDD/DBHelper_ProduitTransport.java
+++ app/src/main/java/com/example/testppe/BDD/DBHelper_ProduitTransport.java
@@ -1,4 +1,74 @@
 package com.example.testppe.BDD;
 
-public class DBHelper_ProduitTransport {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+public class DBHelper_ProduitTransport  extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "MyDBName.db_pste1";
+    public static final String PROJET_TABLE_NAME = "ProduitTransport";
+    Context myContext;
+    SQLiteDatabase myDataBase;
+    private HashMap hp;
+
+    public DBHelper_ProduitTransport (Context context) {
+
+        super(context, DATABASE_NAME , null, 4);
+        myContext = context;
+        myDataBase = this.getReadableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table ProduitTransport" +
+                        "(id_produit integer, id_transport integer,provenance text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+
+        if(newVersion>oldVersion)
+        {onCreate(db);}
+
+        db.execSQL("DROP TABLE IF EXISTS ProduitTransport");
+        onCreate(db);
+    }
+
+    public void insertProduitTransport (int id_util, String re) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id",id_util);
+        contentValues.put("avis",re);
+        db.insert("ProduitTransport", null, contentValues);
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from ProduitTransport where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, PROJET_TABLE_NAME);
+        return numRows;
+    }
+
+
+    public Integer deleteProduitTransport (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("ProduitTransport",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
 }
