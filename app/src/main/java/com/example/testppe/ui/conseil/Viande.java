808c58e434d2d6e5c936de73d29d193185d5690a
leacmrt <lea.commaret@edu.ece.fr>
2021-04-05 21:38:54 +0200

    viande debut

--- app/src/main/java/com/example/testppe/ui/conseil/Viande.java
+++ app/src/main/java/com/example/testppe/ui/conseil/Viande.java
@@ -1,6 +1,8 @@
 package com.example.testppe.ui.conseil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +14,18 @@ import java.util.Date;
 public class Viande  extends AppCompatActivity {
 
     Date currentTime = Calendar.getInstance().getTime();
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viande);
+
+        //bouton retour
+        back =findViewById(R.id.buttonback);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
