53a0b6f9834e7763d6229be583c881dcf77fcdb8
leacmrt <lea.commaret@edu.ece.fr>
2021-04-06 12:49:27 +0200

    ajouter item + viande

--- app/src/main/java/com/example/testppe/AjoutItem.java
+++ app/src/main/java/com/example/testppe/AjoutItem.java
@@ -1,16 +1,25 @@
 package com.example.testppe;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AjoutItem extends AppCompatActivity
 {
+    Button ajouter;
+    TextView code;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ajout);
         String barcodeDATA = getIntent().getStringExtra("EXTRA_SESSION_ID");
+        ajouter = findViewById(R.id.button);
+        code = findViewById(R.id.textCODE);
+
+        code.setText(barcodeDATA);//affichage du code du nouvel objet 
+
     }
 }
 
